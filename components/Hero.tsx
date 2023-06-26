85febec14318584df7bcce0c411bfec69afbd6af
Emma Udeji <gogreneinfo@gmail.com>
2023-06-26 13:53:48 +0100

    updated banner

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,12 +17,11 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, rent a car—quick and super easy!
+        Discover Your Dream Ride: Explore Car Makes, Models, and More!
         </h1>
 
         <p className="hero__subtitle">
-          Streamline your car rental experience with our effortless booking
-          process.
+        Get Instant Access to Car Make, Model, and More with Our User-Friendly Search!
         </p>
 
         <CustomButton
