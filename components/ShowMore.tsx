d453db37f70b595a08b8b380e40057707fa62386
Emma Udeji <gogreneinfo@gmail.com>
2023-06-26 13:08:22 +0100

    temporarily remove dealer sign up

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 
-import { ShowMoreProps } from "@types";
+import { ShowMoreProps } from "@/types";
 import { updateSearchParams } from "@/utils";
 import { CustomButton } from "@/components";
 
